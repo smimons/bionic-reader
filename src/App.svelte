c11aac47d78c301a04c6f09dc86eb9db73c3c30e
Simon Caine <scaine@caci.co.uk>
2022-05-20 13:38:01 +0100

    refactor and cleanliness changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,41 @@
 <script>
   import { marked } from "marked";
   let bionicMode = false;
-  let text = "";
+  let text =
+    "Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic Reading aims to encourage a more in-depth reading and understanding of written content.";
 
   let footer =
-    "**v0.1.** Created by [Simon Caine](https://twitter.com/smimons). Based on the [Bionic Reading](https://bionic-reading.com) concept by [Renato Casutt](https://twitter.com/renato_casutt).";
+    "v0.1. Created by [Simon Caine](https://twitter.com/smimons).\n\nThis app is inspired by, but in no way affiliated, associated, authorized, endorsed by, or in any way officially connected to [bionic-reading.com](https://bionic-reading.com) or its creator, [Renato Casutt](https://twitter.com/renato_casutt).";
 
-  function convertToBionic(text) {
-    return text
-      .split("\n")
-      .map((paragraph) => bionicParagraph(paragraph))
-      .join("\n\n");
-  }
+  const formatText = (text) => {
+    return splitAndFilter(text, "\n").map(formatParagraph).join("\n\n");
+  };
 
-  function bionicParagraph(paragraph) {
-    return paragraph
-      .split(" ")
-      .filter((word) => !!word && word.length > 0)
-      .map((word) => {
-        let midpoint = Math.ceil(word.length / 2);
-        return `**${word.substr(0, midpoint)}**${word.substr(midpoint)}`;
-      })
-      .join(" ");
-  }
+  const formatParagraph = (paragraph) => {
+    return splitAndFilter(paragraph, " ").map(formatWord).join(" ");
+  };
+
+  const formatWord = (word) => {
+    let midpoint = word.length <= 3 ? 1 : Math.round(word.length / 2);
+    return `<b>${word.substr(0, midpoint)}</b>${word.substr(midpoint)}`;
+  };
+
+  const hasLength = (entity) => entity?.length > 0;
+  const splitAndFilter = (text, separator) =>
+    text.split(separator).filter(hasLength);
 </script>
 
 <main>
-  <h1><b>Bionic</b> Reader</h1>
-  <p>
-    An unofficial Bionic Reading 'translator'. Learn more at <a
-      href="https://bionic-reading.com/">bionic-reading.com</a
-    >.
-  </p>
-
+  <h1><b>Bio</b>nic <b>Rea</b>der</h1>
   <label disabled={text.length === 0}>
-    <input
-      type="checkbox"
-      bind:checked={bionicMode}
-      disabled={text.length === 0}
-    />
+    <input type="checkbox" bind:checked={bionicMode} />
     View in Bionic Mode
   </label>
 
   <div class="body">
     {#if !!bionicMode}
       <div class="bionicContainer">
-        {@html marked(convertToBionic(text))}
+        {@html marked(formatText(text))}
       </div>
     {:else}
       <textarea bind:value={text} />
@@ -62,16 +52,12 @@
     max-width: none;
     margin: 0 auto;
     font-size: 1.2rem;
-    color: #333;
+    color: #444;
   }
 
   input[type="checkbox"] {
     transform: scale(1.5);
     margin-right: 0.5rem;
-  }
-
-  a {
-    font-weight: bold;
   }
 
   h1 {
@@ -89,18 +75,19 @@
 
   .bionicContainer {
     text-align: left;
-    min-height: 640px;
+    min-height: 72vh;
     margin: 0 0.5rem;
   }
 
   textarea {
+    margin-top: 0.3rem;
     width: 100%;
     resize: none;
-    height: 640px;
+    height: 72vh;
     color: #333;
   }
 
   footer {
-    font-size: 0.75em;
+    font-size: 0.65em;
   }
 </style>
